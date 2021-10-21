cb92fcc4492723e03ee2ba01a0c5f62cc611c9b4
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-10-22 00:56:07 +0600

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import { slideItems } from '../data';
+import { useState } from 'react';
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 const Arrow = styled.div`
   width: 50px;
@@ -22,6 +25,7 @@ const Arrow = styled.div`
   bottom: 0;
   margin: auto;
   cursor: pointer;
+  z-index: 2;
 
   left: ${(props) => props.direction === 'left' && '10px'};
   right: ${(props) => props.direction === 'right' && '10px'};
@@ -29,12 +33,16 @@ const Arrow = styled.div`
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 const Slide = styled.div`
   display: flex;
   align-items: center;
-  width: 100%;
+  width: 100vw;
   height: 100vh;
+  background-color: #${(props) => props.bg};
 `;
 const ImgContainer = styled.div`
   flex: 1;
@@ -42,37 +50,61 @@ const ImgContainer = styled.div`
 `;
 const Image = styled.img`
   height: 85%;
+  margin-left: 5px;
 `;
 const InfoContainer = styled.div`
   flex: 1;
   padding: 50px;
 `;
-const Title = styled.h1``;
-const Desc = styled.p``;
-const Button = styled.button``;
+const Title = styled.h1`
+  font-size: 60px;
+`;
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 1px;
+`;
+const Button = styled.button`
+  padding: 10px 15px;
+  font-size: 20px;
+  border: none;
+  background: teal;
+  color: white;
+  cursor: pointer;
+  border-radius: 10px;
+`;
 
 function Slider() {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === 'left') {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 3);
+    } else {
+      setSlideIndex(slideIndex < 3 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick('left')}>
         <ArrowLeftIcon />
       </Arrow>
-      <Wrapper>
-        <Slide>
-          <ImgContainer>
-            <Image src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzaGlvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" />
-          </ImgContainer>
-          <InfoContainer>
-            <Title>Western Dress</Title>
-            <Desc>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. At,
-              harum.
-            </Desc>
-            <Button>Cart</Button>
-          </InfoContainer>
-        </Slide>
+      <Wrapper slideIndex={slideIndex}>
+        {slideItems.map((item) => (
+          <Slide bg={item.bg} key={item.id}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>Shop Now</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
       </Wrapper>
-      <Arrow direction="right">
+
+      <Arrow direction="right" onClick={() => handleClick('right')}>
         <ArrowRightIcon />
       </Arrow>
     </Container>
