b1a6f71f9b6ee64cd7abe96ceb7704f9d114a624
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-10-22 00:55:50 +0600

    Create CategoryItem.jsx

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 45vh;
+  position: relative;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const Info = styled.div`
+  position: absolute;
+  top: 35px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.h1`
+  color: #ffffff;
+  margin-bottom: 20px;
+`;
+const Button = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: white;
+  color: gray;
+  cursor: pointer;
+  font-weight: 600;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.5s ease;
+  &:hover {
+    background-color: #f3e2e2;
+    transform: scale(1.2);
+  }
+`;
+
+const CategoryItem = ({ item }) => {
+  return (
+    <Container>
+      <Image src={item.img} />
+      <Info>
+        <Title>{item.title}</Title>
+        <Button>Shop Now</Button>
+      </Info>
+    </Container>
+  );
+};
+
+export default CategoryItem;
