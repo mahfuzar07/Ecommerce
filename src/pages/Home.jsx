959d856832dd5c9685e7c4526838b5228c31ab08
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-10-22 00:56:19 +0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Announcement from '../components/Announcement';
+import Categories from '../components/Categories';
+import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import Newsletter from '../components/Newsletter';
+import Products from '../components/Products';
 import Slider from '../components/Slider';
 
 function Home() {
@@ -9,6 +13,10 @@ function Home() {
       <Announcement></Announcement>
       <Navbar></Navbar>
       <Slider></Slider>
+      <Categories></Categories>
+      <Products></Products>
+      <Newsletter></Newsletter>
+      <Footer></Footer>
     </div>
   );
 }
