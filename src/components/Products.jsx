d58e01bf0e7e82928620880c4366d8608a6b7bbe
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-10-22 00:56:04 +0600

    Create Products.jsx

---
+++ src/components/Products.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { populerProducts } from '../data';
+import Product from './Product';
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+function Products() {
+  return (
+    <Container>
+      {populerProducts.map((item) => (
+        <Product item={item} key={item.id} />
+      ))}
+    </Container>
+  );
+}
+
+export default Products;
