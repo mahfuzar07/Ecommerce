fcd6a8a0a1fb31ebbf38313e67eac1df30f4854b
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-10-22 00:56:11 +0600

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,87 @@
+export const slideItems = [
+  {
+    id: 1,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+    title: 'WINTER  SELL',
+    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. At,',
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+    title: 'SUMMER SELL',
+    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. At,',
+    bg: 'f5fafd',
+  },
+  {
+    id: 3,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+    title: 'Fall SELL',
+    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. At,',
+    bg: 'f5fafd',
+  },
+  {
+    id: 4,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+    title: 'YOUR OFFER',
+    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. At,',
+    bg: 'f5fafd',
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+    title: 'women shirt',
+  },
+  {
+    id: 2,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+    title: 'women pant',
+  },
+  {
+    id: 3,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+    title: 'Men T-Shirt',
+  },
+  {
+    id: 4,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+    title: 'Men Pant',
+  },
+];
+export const populerProducts = [
+  {
+    id: 1,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+  },
+  {
+    id: 2,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+  },
+  {
+    id: 3,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+  },
+  {
+    id: 4,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+  },
+  {
+    id: 5,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+  },
+  {
+    id: 6,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+  },
+  {
+    id: 7,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+  },
+  {
+    id: 8,
+    img: 'https://cdn-magento2-media.head.com/wysiwyg/Gravity_Pull_Carousel_IGcoreFr-1.jpg',
+  },
+];
