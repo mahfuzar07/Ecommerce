90125ef742ab0a18fcb803cff83b455a88f61e2b
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-10-22 21:20:24 +0600

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,11 +6,13 @@ import Navbar from '../components/Navbar';
 import Newsletter from '../components/Newsletter';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import { mobile } from '../responsive';
 
 const Container = styled.div``;
 const Wrapper = styled.div`
   display: flex;
   padding: 50px;
+  ${mobile({ flexDirection: 'column', padding: '10px' })}
 `;
 const ImageContainer = styled.div`
   flex: 1;
@@ -18,11 +20,13 @@ const ImageContainer = styled.div`
 const Image = styled.img`
   width: 100%;
   height: 80vh;
-  object-fit: cover;
+  /* object-fit: cover; */
+  ${mobile({ height: '40vh' })}
 `;
 const InfoContainer = styled.div`
   flex: 1;
   padding: 0px 50px;
+  ${mobile({ padding: '10px' })}
 `;
 const Title = styled.h1`
   font-weight: 300;
@@ -39,6 +43,7 @@ const FilterContainer = styled.div`
   margin: 30px 0px;
   display: flex;
   justify-content: space-between;
+  ${mobile({ width: '100%' })}
 `;
 const Filter = styled.div`
   display: flex;
@@ -66,6 +71,7 @@ const AddContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({ width: '100%' })}
 `;
 const AmountContainer = styled.div`
   display: flex;
